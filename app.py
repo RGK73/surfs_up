fe43392d0e78524613350510a921e02b6f81b15e
Revati Kulkarni <revatiikatkar@yahoo.com>
2022-02-09 09:27:48 -0500

    climate analysis files

--- app.py
+++ app.py
@@ -1,26 +1,41 @@
-from flask import Flask
-from datetime import datetime
-import re
+import datetime as dt
+import numpy as np
+import pandas as pd
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
 app = Flask(__name__)
+# import app
 
-@app.route("/test")
-def hello_world():
-    return 'Hello world'
+# print("example __name__ = %s", __name__)
 
-@app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+# if __name__ == "__main__":
+#     print("example is being run directly.")
+# else:
+#     print("example is being imported")
 
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
+@app.route("/")
+# def welcome():
+#     return
 
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
